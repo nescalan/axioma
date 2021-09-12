67ed79767a7f77bf5d2141e5846f4302b9d8cd8b
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-09-12 15:36:15 -0600

    Main menu

--- js/login.js
+++ js/login.js
@@ -1,11 +1,41 @@
-const signUpButton = document.getElementById("signUp");
+// Declaracion de Variables
+const iniciarSesion = document.getElementById("iniciar-sesion");
+const inscribirse = document.getElementById("inscribirse");
 const signInButton = document.getElementById("signIn");
+const signUpButton = document.getElementById("signUp");
 const container = document.getElementById("container");
+let usuario = document.getElementById("usuario");
+let clave = document.getElementById("clave");
+let user = "admin@email.com";
+let pass = "password";
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+// Funciones
+const validar = () => {
+  const mainMenu = "/app/main-menu.html";
+  if (usuario.value === "") {
+    console.log("el campo de usuario es obligatorio");
+    evt.preventDefault();
+    return false;
+  }
+  if (clave.value === "") {
+    console.log("el campo contraseña es obligatorio");
+    evt.preventDefault();
+    return false;
+  }
+  if (usuario.value == user && clave.value == pass) {
+    console.log("Exito");
+    window.location.replace("/app/main-menu.html");
+  }
+};
+
+// Listeners del boton para Iniciar Sesión
+iniciarSesion.addEventListener("click", validar);
 
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+// Listeners del boton para Inscribirse
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
