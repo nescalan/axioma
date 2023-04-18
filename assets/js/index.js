0babc7a983aaaa2a3fbea3beb7641666b2e02d50
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-04-18 11:14:54 -0600

    Agregada rutina de validación para ingreso de nombres de usuario
    En este commit se agregó una nueva función de validación en el sistema, la cual verifica si el nombre de usuario ingresado consta únicamente de números y también comprueba que la longitud del nombre de usuario no sea menor a 9 dígitos. Esto se logró a través de la implementación de una expresión regular que filtra y verifica el formato de entrada del usuario antes de ser almacenado en la base de datos.
    
    Con esta mejora en la validación de datos, se garantiza que solo se almacenen nombres de usuario válidos y que cumplan con los requisitos mínimos de seguridad establecidos por la empresa.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,47 @@
 $(document).ready(function () {
   $("#login-form").on("change", "#username", function () {
-    console.log("Is changing");
+    const parent = $(this).parent();
+    const domUserName = $(this).val();
+    console.log(domUserName);
+    const domErrorMessage = parent.find("#error-message");
+    let errorMessage;
+
+    // Validate the username using regular expressions
+    if (!/^\d+$/.test(domUserName)) {
+      errorMessage =
+        "Por favor, ingrese un número de identificación válido sin guiones";
+      console.log(errorMessage);
+      domErrorMessage.fadeIn();
+      errorFlag = true;
+    } else {
+      // Validate the username length
+      if (domUserName.length < 9) {
+        console.log(domUserName.length);
+        errorMessage = "Revise la cantidad de digitos ingresada";
+        console.log(errorMessage);
+        domErrorMessage.fadeIn();
+        errorFlag = true;
+      } else {
+        domErrorMessage.fadeOut();
+        console.log(domUserName.length);
+      }
+
+      if (errorFlag) {
+        //CSS: Styles for errorMessage
+        domErrorMessage.html(errorMessage).css({
+          margin: "20px 0",
+          padding: "15px 35px 15px 15px",
+          color: "#b94a48",
+          "font-size": "14px",
+          "line-height": "20px",
+          "border-color": "#eed3d7",
+          "border-radius": "4px",
+          "border-style": "solid",
+          "border-width": "1px",
+          backgroundColor: "#f2dede",
+        });
+      }
+    }
   });
 
   // $("#login-form").on("click", "#btn-login", function (event) {
@@ -8,44 +49,18 @@ $(document).ready(function () {
   //   const parent = $(this).parent();
   //   const domUserName = $("#username").val();
   //   const domPassword = $("#password").val();
-  //   let domErrorMessage = parent.find("#error-message").html();
 
-  //   // domErrorMessage.text("Fuck error");
-  //   //Shows Error Message
-
-  //   console.log(
-  //     `user: ${domUserName} | Pwd: ${domPassword} | Error: ${domErrorMessage}`
-  //   );
+  //   //Shows User Name and Error Message
+  //   console.log(`user: ${domUserName} | Pwd: ${domPassword} |`);
 
   //   if (domUserName.length == 0 || domPassword.length == 0) {
   //     errorMessage = "Debe rellenar todos los campos";
-  //     $(domErrorMessage).html("Error");
   //   }
 
-  //   // // Validate the username using regular expressions
-  //   // if (!/^\d+$/.test(domUserName)) {
-  //   //   errorMessage = "El nombre de usuario debe contener solo números";
-  //   //   domErrorMessage.fadeIn();
-  //   //   return false;
-  //   // }
-
-  //   // // Validate the password length
-  //   // if (domPassword.length < 8) {
-  //   //   errorMessage = "La contraseña debe tener al menos 8 caracteres";
-  //   //   domErrorMessage.fadeIn();
-  //   //   return false;
-  //   // }
-  //   // domErrorMessage.html(errorMessage).css({
-  //   //   margin: "0px 0px 20px",
-  //   //   padding: "15px 35px 15px 15px",
-  //   //   color: "#b94a48",
-  //   //   "font-size": "14px",
-  //   //   "line-height": "20px",
-  //   //   "border-color": "#eed3d7",
-  //   //   "border-radius": "4px",
-  //   //   "border-style": "solid",
-  //   //   "border-width": "1px",
-  //   //   backgroundColor: "#f2dede",
-  //   // });
+  //   // Validate the password length
+  //   if (domPassword.length < 8) {
+  //     errorMessage = "La contraseña debe tener al menos 8 caracteres";
+  //     return false;
+  //   }
   // });
 });
