04d87b1a376021f9dd2d0d2de46136bd6385a192
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-05-22 15:16:11 -0600

    FIX: Visitas Table
    + hora_ingreso TIME
    + hora_salida TIM

--- database_files/1 Create/create_table_visitas.sql
+++ database_files/1 Create/create_table_visitas.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS visitas(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fecha_ingreso DATE NOT NULL,
+    hora_ingreso TIME,
     fecha_salida DATE NOT NULL,
+    fecha_salida TIME,
     tipo_visita VARCHAR(60) NOT NULL,
     placa_vehiculo VARCHAR(7) NOT NULL,
     numero_acompanantes INT UNSIGNED NOT NULL,
